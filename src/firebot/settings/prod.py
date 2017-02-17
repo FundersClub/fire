78343a57e3123fd38bb67da3e099ddadba666e21
Eran Rundstein <e@keves.org>
2017-02-16 17:20:31 -0800

    yay logging

--- src/firebot/settings/prod.py
+++ src/firebot/settings/prod.py
@@ -1,4 +1,5 @@
 import dj_database_url
+import os
 
 from firebot.settings.base import *  # noqa
 
@@ -20,10 +21,50 @@ INSTALLED_APPS += (
     'raven.contrib.django.raven_compat',
 )
 
-import os
-import raven
-
 RAVEN_CONFIG = {
     'dsn': 'https://ec78c1fd42f144e2aca4facf5a012e87:205e3c07dfb44cbab804e6c40026d268@sentry.io/139935',
     'release': os.environ.get('HEROKU_SLUG_COMMIT'),
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
 }
