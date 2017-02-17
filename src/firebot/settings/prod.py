cd65afc04dcd6363758b15be3a6e63335ce04d3c
Eran Rundstein <e@keves.org>
2017-02-16 16:59:49 -0800

    add CELERY_BROKER_URL to prod settings

--- src/firebot/settings/prod.py
+++ src/firebot/settings/prod.py
@@ -4,6 +4,7 @@ from firebot.settings.base import *  # noqa
 
 
 ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(',')
+CELERY_BROKER_URL = os.environ['REDIS_URL']
 DEBUG = os.environ.get('DJANGO_DEBUG') == 'YES'
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
