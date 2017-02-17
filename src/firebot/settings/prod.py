519523ad49acfd9427f01d78fdb2083d6c04e86a
Eran Rundstein <e@keves.org>
2017-02-16 16:43:50 -0800

    config updates

--- src/firebot/settings/prod.py
+++ src/firebot/settings/prod.py
@@ -3,9 +3,10 @@ import dj_database_url
 from firebot.settings.base import *  # noqa
 
 
+ALLOWED_HOSTS = ','.split(os.environ['DJANGO_ALLOWED_HOSTS'])
+DEBUG = False
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': dj_database_url.parse(
