ac42aaa456c30823801ea7e2b9ac86ea0fcb46ca
Eran Rundstein <e@keves.org>
2017-02-16 16:13:47 -0800

    heroku

--- src/firebot/settings/prod.py
+++ src/firebot/settings/prod.py
@@ -2,11 +2,14 @@ import dj_database_url
 
 from firebot.settings.base import *  # noqa
 
+
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ['DJANGO_DB'],
+        os.environ['DATABASE_URL'],
         conn_max_age=int(os.environ.get('DJANGO_DB_CONN_MAX_AGE', 0)),
     ),
 }
