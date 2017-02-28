d2fa9efe62e4e6734d7a96dbdafa13741a322384
Eran Rundstein <e@keves.org>
2017-02-28 14:16:01 -0800

    Add "purge all data" ability, surface in API (fix #19)

--- src/fb_github/api.py
+++ src/fb_github/api.py
@@ -8,7 +8,9 @@ from rest_framework import (
     permissions,
     routers,
     serializers,
+    status,
 )
+from rest_framework.decorators import detail_route
 from rest_framework.mixins import (
     CreateModelMixin,
     DestroyModelMixin,
@@ -21,6 +23,7 @@ from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
+from fb_emails import models as fb_emails_models
 from fb_github import models
 from firebot.api import BaseSerializer
 
@@ -44,6 +47,7 @@ class EmailMapSerializer(BaseSerializer):
 
 class RepositorySerializer(serializers.HyperlinkedModelSerializer):
     emailmap_set = EmailMapSerializer(many=True, read_only=True)
+    urls = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Repository
@@ -54,6 +58,7 @@ class RepositorySerializer(serializers.HyperlinkedModelSerializer):
             'name',
             'status',
             'url',
+            'urls',
         )
         read_only_fields = [f for f in fields if f != 'email_slug']
         extra_kwargs = {
@@ -66,6 +71,11 @@ class RepositorySerializer(serializers.HyperlinkedModelSerializer):
         if value in settings.FIREBOT_BANNED_EMAIL_SLUGS:
             raise serializers.ValidationError('"{}" is not permitted.'.format(value))
         return value
+
+    def get_urls(self, obj):
+        return {
+            'purge_attachments': reverse('repository-purge-attachments', args=[obj.pk], request=self.context['request']),
+        }
 
 
 ################################################################################
@@ -103,6 +113,19 @@ class RepositoryViewSet(
         return (super(RepositoryViewSet, self).get_queryset()
             .filter(admins=self.request.user)
         )
+
+    @detail_route(methods=['post'])
+    def purge_attachments(self, request, pk=None):
+        repo = self.get_object()
+
+        qs = fb_emails_models.Attachment.objects.filter(
+            msg__issue__repo=repo
+        )
+        for attachment in qs.iterator():
+            attachment.file.delete()
+            attachment.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 ################################################################################
