356bc9359f8278d97d0d0e50fdcd3b2f741542fa
Charley Walton <chazeah@gmail.com>
2017-02-28 14:42:59 -0800

    styles for list view

--- src-frontend/app/repository/repository-list.component.ts
+++ src-frontend/app/repository/repository-list.component.ts
@@ -4,6 +4,7 @@ import { RepositoryService } from './repository.service';
 
 @Component({
     selector: 'repository-list',
+    styleUrls: ['./repository-list.component.css'],
     templateUrl: './repository-list.component.html',
 })
 export class RepositoryListComponent {
