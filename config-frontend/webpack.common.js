77ba66acc272bc5cfcb952287ebbe2c22754113a
Andrew Jaeger <mrjaeger00@gmail.com>
2017-03-21 13:41:02 -0400

    First pass at trying to get AOT working

--- config-frontend/webpack.common.js
+++ config-frontend/webpack.common.js
@@ -3,6 +3,8 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 var helpers = require('./helpers');
+var ngtools = require('@ngtools/webpack');
+var path = require('path');
 
 module.exports = {
     entry: {
@@ -20,38 +22,42 @@ module.exports = {
         rules: [
             {
                 test: /\.ts$/,
-                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+                use: ['awesome-typescript-loader', 'angular2-template-loader']
             },
             {
                 test: /\.html$/,
-                loader: 'html-loader'
+                use: 'html-loader'
             },
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+                use: 'file-loader?name=assets/[name].[hash].[ext]'
             },
             {
                 test: /\.scss$/,
                 include: helpers.root('src-frontend', 'app'),
-                loaders: ['raw-loader', 'sass-loader']
+                use: ['raw-loader', 'sass-loader']
             },
             {
                 test: /\.css$/,
                 exclude: helpers.root('src-frontend', 'app'),
-                loader: ExtractTextPlugin.extract({
-                    fallbackLoader: 'style-loader',
-                    loader: 'css-loader?sourceMap'
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
                 })
             },
             {
                 test: /\.css$/,
                 include: helpers.root('src-frontend', 'app'),
-                loader: 'raw-loader'
+                use: 'raw-loader'
             }
         ]
     },
 
     plugins: [
+        new ngtools.AotPlugin({
+            tsConfigPath: path.join(process.cwd(), 'tsconfig.json')
+        }),
+
         // Workaround for angular/angular#11580
         new webpack.ContextReplacementPlugin(
             // The (\\|\/) piece accounts for path separators in *nix and Windows
