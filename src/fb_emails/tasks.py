43e259d0bfdd8bfc21a84ee1a7124a5d738a962e
Eran Rundstein <e@keves.org>
2018-07-24 10:55:55 -0700

    make email lookup case insensitive

--- src/fb_emails/tasks.py
+++ src/fb_emails/tasks.py
@@ -69,9 +69,9 @@ def process_incoming_message(msg_id):
 
     # If we don't recognize this email address and haven't seen it before,
     # offer user to associate their GitHub account with this repo
-    email_recognized = repo.emailmap_set.filter(email=msg.from_email).exists()
+    email_recognized = repo.emailmap_set.filter(email__iexact=msg.from_email).exists()
     email_seen_before = IncomingMessage.objects.filter(
-        issue__repo=repo, from_email=msg.from_email
+        issue__repo=repo, from_email__iexact=msg.from_email
     ).exclude(id=msg.id).exists()
     if not email_recognized and not email_seen_before:
         msg.reply_from_template('fb_emails/offer-associate.html', {
