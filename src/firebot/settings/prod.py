319e2c5aac86aa162a76134ba79830e716c6884e
Eran Rundstein <eran@rundste.in>
2021-01-28 19:43:30 -0800

    upgrade sentry sdk

--- src/firebot/settings/prod.py
+++ src/firebot/settings/prod.py
@@ -29,57 +29,19 @@ DATABASES = {
     ),
 }
 
-INSTALLED_APPS += (  # noqa
-    'raven.contrib.django.raven_compat',
-)
+if os.environ.get('SENTRY_DSN'):
+    import sentry_sdk
+    from sentry_sdk.integrations.celery import CeleryIntegration
+    from sentry_sdk.integrations.django import DjangoIntegration
 
-RAVEN_CONFIG = {
-    'dsn': os.environ.get('SENTRY_DSN'),
-    'release': os.environ.get('HEROKU_SLUG_COMMIT'),
-}
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'WARNING',
-        'handlers': ['sentry', 'console'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s '
-                      '%(process)d %(thread)d %(message)s'
-        },
-    },
-    'handlers': {
-        'sentry': {
-            'level': 'ERROR',
-            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-    },
-}
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN'),
+        release=os.environ.get('HEROKU_SLUG_COMMIT'),
+        integrations=[CeleryIntegration(), DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True,
+        attach_stacktrace=True,
+    )
 
 
 ###############################################################################
